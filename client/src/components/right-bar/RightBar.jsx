f9721db1c9bb9efdca9d77aa28e06adde83e900b
Matcha7007 <matchaaddict007@gmail.com>
2023-05-26 10:45:50 +0700

    Design Rightbar

---
+++ client/src/components/right-bar/RightBar.jsx
@@ -0,0 +1,239 @@
+import './rightbar.scss'
+
+const RightBar = () => {
+  return (
+    <div className="rightBar">
+      <div className="container">
+        <div className="item">
+          <span>Suggestion For You</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <span>Dapa</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <span>Dapa</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <span>Lates Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <span>Dapa</span> changed their cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>   
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <span>Dapa</span> changed their cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>   
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <span>Dapa</span> changed their cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>   
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <span>Dapa</span> changed their cover picture
+              </p>
+            </div>
+            <span>1 min ago</span>   
+          </div>
+        </div>
+        <div className="item">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <div className="online" />
+                <span>Dapa</span>
+              </p>
+            </div>  
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <div className="online" />
+                <span>Dapa</span>
+              </p>
+            </div>  
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <div className="online" />
+                <span>Dapa</span>
+              </p>
+            </div>  
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <div className="online" />
+                <span>Dapa</span>
+              </p>
+            </div>  
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <div className="online" />
+                <span>Dapa</span>
+              </p>
+            </div>  
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <div className="online" />
+                <span>Dapa</span>
+              </p>
+            </div>  
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <div className="online" />
+                <span>Dapa</span>
+              </p>
+            </div>  
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <div className="online" />
+                <span>Dapa</span>
+              </p>
+            </div>  
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <div className="online" />
+                <span>Dapa</span>
+              </p>
+            </div>  
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <div className="online" />
+                <span>Dapa</span>
+              </p>
+            </div>  
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <div className="online" />
+                <span>Dapa</span>
+              </p>
+            </div>  
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <div className="online" />
+                <span>Dapa</span>
+              </p>
+            </div>  
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <div className="online" />
+                <span>Dapa</span>
+              </p>
+            </div>  
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <div className="online" />
+                <span>Dapa</span>
+              </p>
+            </div>  
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <div className="online" />
+                <span>Dapa</span>
+              </p>
+            </div>  
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <div className="online" />
+                <span>Dapa</span>
+              </p>
+            </div>  
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <div className="online" />
+                <span>Dapa</span>
+              </p>
+            </div>  
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" />
+              <p>
+                <div className="online" />
+                <span>Dapa</span>
+              </p>
+            </div>  
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default RightBar
