3cc16c61ff18c918bb2ebc4267b4ec79c91c3fe1
Matcha7007 <matchaaddict007@gmail.com>
2023-05-29 13:29:56 +0700

    Comment UI Design

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -5,49 +5,52 @@ import TextsmsOutlinedIcon from '@mui/icons-material/TextsmsOutlined';
 import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import { Link } from 'react-router-dom'
+import Comments from '../comments/Comments';
+import { useState } from 'react';
 
 const Post = ({post}) => {
+    const [commentOpen, setCommentOpen] = useState(false)
 
     //Temporary
-
     const liked = false
 
-  return (
-    <div className="post">
-        <div className="container">
-            <div className="user">
-                <div className="userInfo">
-                    <img src={post.profilePic} alt="" />
-                    <div className="details">
-                        <Link to={`/profile/${post.userId}`} style={{ textDecoration:"none", color:"inherit" }}>
-                            <span className='name'>{post.name}</span>
-                        </Link>
-                        <span className='date'>1 min ago</span>
+    return (
+        <div className="post">
+            <div className="container">
+                <div className="user">
+                    <div className="userInfo">
+                        <img src={post.profilePic} alt="" />
+                        <div className="details">
+                            <Link to={`/profile/${post.userId}`} style={{ textDecoration:"none", color:"inherit" }}>
+                                <span className='name'>{post.name}</span>
+                            </Link>
+                            <span className='date'>1 min ago</span>
+                        </div>
+                    </div>
+                    <MoreHorizIcon />
+                </div>
+                <div className="content">
+                    <p>{post.desc}</p>
+                    <img src={post.img} alt="" />
+                </div>
+                <div className="info">
+                    <div className="item">
+                        { liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon /> } 
+                        12 Likes
+                    </div>
+                    <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
+                        <TextsmsOutlinedIcon />
+                        1 Comment
+                    </div>
+                    <div className="item">
+                        <ShareOutlinedIcon />
+                        Share
                     </div>
                 </div>
-                <MoreHorizIcon />
-            </div>
-            <div className="content">
-                <p>{post.desc}</p>
-                <img src={post.img} alt="" />
-            </div>
-            <div className="info">
-                <div className="item">
-                    { liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon /> } 
-                    12 Likes
-                </div>
-                <div className="item">
-                    <TextsmsOutlinedIcon />
-                    1 Comment
-                </div>
-                <div className="item">
-                    <ShareOutlinedIcon />
-                    Share
-                </div>
+                { commentOpen && <Comments />}
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Post
